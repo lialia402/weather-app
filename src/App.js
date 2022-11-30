262801e26b9930a964ae58cc83dd0ff31eba4d7c
lialia402 <lialia402@gmail.com>
2022-11-30 14:32:47 +0200

    search component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Search from './components/Search/Search';
 
 function App() {
+  
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+    // const [lat, lon] = searchData.value.split(" ");
+
+    // const currentWeatherFetch = fetch(
+    //   `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    // );
+    // const forecastFetch = fetch(
+    //   `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    // );
+
+    // Promise.all([currentWeatherFetch, forecastFetch])
+    //   .then(async (response) => {
+    //     const weatherResponse = await response[0].json();
+    //     const forcastResponse = await response[1].json();
+
+    //     setCurrentWeather({ city: searchData.label, ...weatherResponse });
+    //     setForecast({ city: searchData.label, ...forcastResponse });
+    //   })
+    //   .catch(console.log);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "88c3f7a200msh45e10ab9fa9c8e4p1aaa12jsnd3c1ff733a26",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "40323ce27bef5b576b80946da1fc998d"; 
