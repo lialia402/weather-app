12e13a55043c4922ac167fedb80b1922f9b9af32
lialia402 <lialia402@gmail.com>
2022-12-01 14:39:08 +0200

    minor fixes

--- src/components/Forcast/Forcast.jsx
+++ src/components/Forcast/Forcast.jsx
@@ -31,28 +31,28 @@ const Forcast = ({ data }) => {
                             </AccordionItemButton>
                         </AccordionItemHeading>
                         <AccordionItemPanel>
-                            <div className="daily-details-grid">
-                                <div className="daily-details-grid-item">
+                            <div className="daily-details">
+                                <div className="daily-details-item">
                                     <label>Pressure:</label>
                                     <label>{item.main.pressure}</label>
                                 </div>
-                                <div className="daily-details-grid-item">
+                                <div className="daily-details-item">
                                     <label>Humidity:</label>
                                     <label>{item.main.humidity}</label>
                                 </div>
-                                <div className="daily-details-grid-item">
+                                <div className="daily-details-item">
                                     <label>Clouds:</label>
                                     <label>{item.clouds.all}%</label>
                                 </div>
-                                <div className="daily-details-grid-item">
+                                <div className="daily-details-item">
                                     <label>Wind speed:</label>
                                     <label>{item.wind.speed} m/s</label>
                                 </div>
-                                <div className="daily-details-grid-item">
+                                <div className="daily-details-item">
                                     <label>Sea level:</label>
                                     <label>{item.main.sea_level}m</label>
                                 </div>
-                                <div className="daily-details-grid-item">
+                                <div className="daily-details-item">
                                     <label>Feels like:</label>
                                     <label>{item.main.feels_like}°C</label>
                                 </div>
